13c950b20bc417724b6823252ee3c3308f4445ca
Mike <mike@pc-249.home>
2026-03-30 18:30:32 +0200

    Starting calc_q_n.py

---
+++ src/calc_q_n.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+#  calc_q_n.py
+#  
+#  Copyright 2026 Mike <mike@Fedora40>
+#  
+#  
+
+
+import sys
+import math
+import itertools as it
+from sympy import ntheory
+
+def Qn(n):
+	# Define Q(n) to be the smallest number which occurs in exactly n Pythagorean Triples
+	# Examples Q(5) = 15, Q(10) = 48 and Q(10^3) = 8064000
+	# Given a number e.g. a = 75
+	# find the number of triples when 75 is a 'leg'
+	nlegs = triples_legs(n)
+	nhyps = triples_hypot(n)
+	total = nlegs + nhyps
+	print(f"nlegs = {nlegs} nhypn = {nhyps} total = {total}")
+	return total
+
+def main(args):
+		Qn(75)	# expect 7 + 2 = 9
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
