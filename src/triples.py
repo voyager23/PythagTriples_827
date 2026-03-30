67f7ef7a1c5acdca23a12ab3a3e9775a66ed5215
Mike <mike@pc-249.home>
2026-03-30 08:59:36 +0200

    Anomally between triples.py and basicdata.y

--- src/triples.py
+++ src/triples.py
@@ -27,8 +27,25 @@ for m in range(2, limit):
             k += 1
 
 # Print sorted triples
-triples = sorted(triples)
-for t in triples:
-    print(t)
+# ~ triples = sorted(triples)
+# ~ for t in triples:
+    # ~ print(t)
 
 print("Total triples:", len(triples))
+
+# Using set of triples count/show triples containing query integer
+query = int(input("Enter query: "))
+count = 0
+for t in triples:
+	if query in list(t):
+		count += 1
+		print(t)
+print(f"Found {count} triples containing {query}")
+ 
+ 
+"""
+From basicdata:
+	[9, 75] -> Q(9) = 75
+	analysis and triples.py return a count of 8 for integer 75
+"""
+
