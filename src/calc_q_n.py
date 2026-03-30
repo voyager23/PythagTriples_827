12600990d89d9c224d02bb6589fd23f7100c2148
Mike <mike@pc-249.home>
2026-03-30 18:41:04 +0200

    Restore point

--- src/calc_q_n.py
+++ src/calc_q_n.py
@@ -12,20 +12,27 @@ import math
 import itertools as it
 from sympy import ntheory
 
+def triples_legs(n):
+	return ntheory.factorint(n)
+	
+def triples_hypot(n):
+	return ntheory.factorint(n)
+
 def Qn(n):
 	# Define Q(n) to be the smallest number which occurs in exactly n Pythagorean Triples
 	# Examples Q(5) = 15, Q(10) = 48 and Q(10^3) = 8064000
 	# Given a number e.g. a = 75
 	# find the number of triples when 75 is a 'leg'
 	nlegs = triples_legs(n)
-	nhyps = triples_hypot(n)
-	total = nlegs + nhyps
+	nhyps = triples_hypot(n+5)
+	#total = nlegs + nhyps
+	total = -1
 	print(f"nlegs = {nlegs} nhypn = {nhyps} total = {total}")
 	return total
 
 def main(args):
-		Qn(75)	# expect 7 + 2 = 9
-    return 0
+	Qn(75)	# expect 7 + 2 = 9
+	return 0
 
 
 if __name__ == '__main__':
