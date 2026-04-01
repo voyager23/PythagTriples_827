b5fd5a19a07783f2075285f9e9273a15e572ab4e
Mike <mike@pc-249.home>
2026-04-01 11:31:06 +0200

    Modifed calc_q_n to use numpy.array(). Berggren and Price matricies tested.

--- src/calc_q_n.py
+++ src/calc_q_n.py
@@ -11,6 +11,7 @@ import sys
 import math
 import itertools as it
 from sympy import ntheory
+import numpy as np
 
 def triples_legs(n):
 	# list divisor pairs for n
@@ -35,9 +36,46 @@ def Qn(n):
 	total = -1
 	print(f"nlegs = {nlegs} nhypn = {nhyps} total = {total}")
 	return total
-
+	
+def ptt_berggren(Z):
+	# Z is a column vector [[a],[b],[c]]
+	# Define 3 transformation matrix
+	#Z = np.array([[a],[b],[c]])
+	A = np.array([[-1,2,2],[-2,1,2],[-2,2,3]])
+	B = np.array([[1,2,2],[2,1,2],[2,2,3]])
+	C = np.array([[1,-2,2],[2,-1,2],[2,-2,3]])
+	ptt1 = np.reshape(np.dot(A,Z),3)
+	ptt2 = np.reshape(np.dot(B,Z),3)
+	ptt3 = np.reshape(np.dot(C,Z),3)
+	return [ptt1, ptt2, ptt3]
+	
+def ptt_price(Z):
+	# Z is a column vector [[a],[b],[c]]
+	# Define 3 transformation matrix
+	#Z = np.array([[a],[b],[c]])
+	A = np.array([[2,1,-1],[-2,2,2],[-2,1,3]])
+	B = np.array([[2,1,1],[2,-2,2],[2,-1,3]])
+	C = np.array([[2,-1,1],[2,2,2],[2,1,3]])
+	ptt1 = np.reshape(np.dot(A,Z),3)
+	ptt2 = np.reshape(np.dot(B,Z),3)
+	ptt3 = np.reshape(np.dot(C,Z),3)
+	return [ptt1, ptt2, ptt3]
+	
+	
 def main(args):
-	Qn(75)	# expect 7 + 2 = 9
+	Y = np.asarray([3,4,5])
+	ptt = ptt_berggren(Y)
+	print("Base triple ",Y)
+	
+	for pt in ptt:
+		print()
+		print(f"From Berggren {pt}")
+		pt_price = ptt_price(pt)
+		print("From Price:", end='')
+		for ptp in pt_price:
+			print(ptp, end=' ')
+		print()
+		
 	return 0
 
 
